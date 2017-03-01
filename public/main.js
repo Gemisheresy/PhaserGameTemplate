6a1c27af2f1b57046a4fb07f3c047373372bddb6
Gemisheresy <Gemisheresy@gmail.com>
2017-03-01 17:49:23 -0500

    First Commit

---
+++ public/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gemis on 2/28/17.
+ */
+var mainState = {
+    preload: function(){
+
+
+    },
+    create: function(){
+
+    },
+    update: function(){
+
+    },
+
+};
+var game = new Phaser.Game(400,300,Phaser.AUTO,"gameDiv");
+game.state.add("main",mainState);
+game.state.start('main');
